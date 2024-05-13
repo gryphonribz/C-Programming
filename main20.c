480ba93126f01aab9f31249c24b373c90a2e8abf
gryphonribz <gryphonribz@gmail.com>
2024-05-13 22:06:23 +0600

    Improved

--- main20.c
+++ main20.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
-    int main() {
-        int num_one,num_two;
 
+    int main() {
+        int num_one, num_two;
+    
         printf("Please Enter a Number: ");
         scanf("%d", &num_one);
-
+    
         printf("Please Enter Another Number: ");
         scanf("%d", &num_two);
-
-        int value_one,value_two,value_three,value_four;
-        
-        value_one = num_one + num_two;
-        value_two = num_one - num_two;
-        value_three = num_one * num_two;
-        value_four = num_one / num_two;
-
-        char sign_one,sign_two,sign_three,sign_four;
-
-        sign_one = '+';
-        sign_two = '-';
-        sign_three = '*';
-        sign_four = '/';
-
-        printf("%d %c %d = %d\n", num_one,sign_one,num_two,value_one);
-        printf("%d %c %d = %d\n", num_one,sign_two,num_two,value_two);
-        printf("%d %c %d = %d\n", num_one,sign_three,num_two,value_three);
-        printf("%d %c %d = %d\n", num_one,sign_four,num_two,value_four);
-        
+    
+        int value_one = num_one + num_two;
+        int value_two = num_one - num_two;
+        int value_three = num_one * num_two;
+        int value_four = num_one / num_two;
+    
+        char sign_one = '+';
+        char sign_two = '-';
+        char sign_three = '*';
+        char sign_four = '/';
+    
+        printf("%d %c %d = %d\n", num_one, sign_one, num_two, value_one);
+        printf("%d %c %d = %d\n", num_one, sign_two, num_two, value_two);
+        printf("%d %c %d = %d\n", num_one, sign_three, num_two, value_three);
+        printf("%d %c %d = %d\n", num_one, sign_four, num_two, value_four);
+    
         return 0;
     }
     
